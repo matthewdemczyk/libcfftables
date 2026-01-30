9577f20677dd6c59de936d2dd1add25f3d3b2840
Matthew Demczyk <matthewdemczyk@gmail.com>
2026-01-30 02:22:10 -0500

    Fix gcovr problem with too many function calls in new unit tests

--- build_and_test_locally.sh
+++ build_and_test_locally.sh
@@ -6,6 +6,7 @@ cd build
 cmake .. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_FLAGS="--coverage"
 make
 ctest --output-on-failure
-gcovr -r .. \
+gcovr --gcov-ignore-parse-errors=suspicious_hits.warn_once_per_file \
+    -r .. \
     --filter '.*/src/.*\.c$' \
     --html --html-details -o coverage.html
